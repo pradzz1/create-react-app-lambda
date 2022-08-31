357bd5f7054749a330a02c6cdbf2e2a4152c67b3
pradzz1 <pradzz.rawat6@gmail.com>
2022-08-31 23:36:09 +0530

    first commitk

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -29,7 +29,7 @@ export default function Dashboard() {
 
 useEffect(() => {
     // Update the document title using the browser API
-    const data =  fetch("/.netlify/functions/async-dadjoke")
+     fetch("/.netlify/functions/async-dadjoke")
       .then(response => console.log(response.json()))
       
   
